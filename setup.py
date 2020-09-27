cde5668369eee7aa80492880fbaeee4f5a6d5cdb
watakandai <kandai.wata@gmail.com>
2020-09-27 20:27:22 +0000

    Deploying to gh-pages from  @ cac67f1510d53a938e613d75eb76dd6df95358f7 🚀

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['hiro_ros_arm_controller'],
-    package_dir={'': 'script'}
-)
-setup(**d)
