92ea13f9d89e5ced3505df8c5890d4b7cce4fee1
calebescobedo <37485219+calebescobedo@users.noreply.github.com>
2021-01-31 20:42:01 +0000

    Deploying to gh-pages from  @ 974e6b1a0482c81473f01871c1dcfd0246cb3033 🚀

--- menudata.js
+++ menudata.js
@@ -64,4 +64,7 @@ var menudata={children:[
 {text:"y",url:"functions_vars.html#index_y"},
 {text:"z",url:"functions_vars.html#index_z"}]}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Functions",url:"globals_func.html"}]}]}]}
